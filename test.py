c0a15825cc496c708f10e7482a6675314dfc420b
SciWilro <mr.wilro@gmail.com>
2022-08-31 09:31:04 +0200

    First Commit

---
+++ test.py
@@ -0,0 +1,14 @@
+"""
+Just a test
+"""
+
+__author__ = "Wilro - https://github.com/SciWilro"
+
+
+def main():
+    """Main entry point of the app"""
+    print("VSCode - Git working... Maybe")
+
+
+if __name__ == "__main__":
+    main()
